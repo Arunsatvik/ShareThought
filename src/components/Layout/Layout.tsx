3c4b23e3dfa45efc5cf3907d84e8ee9364580566
Arunsatvik <satvikmpalli2000@gmail.com>
2023-07-17 19:44:57 -0700

    Initial commit

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Layout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+// import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import { theme } from '../chakra/theme'
+import Layout from '../components/Layout/Layout';
+
+function MyApp({ Component, pageProps }:AppProps){
+  return (
+    <ChakraProvider theme = {theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp
